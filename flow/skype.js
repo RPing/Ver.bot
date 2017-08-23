23d389ac25e328991f3bc4f53ff7f0e0ce383c80
Stephen Chen <g1222888@gmail.com>
2017-08-23 17:34:32 +0800

    [Skype] subscribe command

--- flow/skype.js
+++ flow/skype.js
@@ -1,5 +1,20 @@
-const msg = require('./message')
 const skypeTemplate = require('claudia-bot-builder').skypeTemplate
+const msg = require('./message')
+const db = require('../lib/db')
+const site = require('../lib/site-utils')
+const error = require('../lib/error')
+
+const projectPlatforms = Object.keys(msg.EXAMPLE_URL)
+
+function promiseErrorHandler(err) {
+    console.error(err)
+
+    const errorMsgKey = error.getErrorType(err)
+    return [
+        msg[errorMsgKey] || msg.UNKNOWN_ERROR,
+        msg.COMMAND_LIST
+    ]
+}
 
 function flow(message, originalApiRequest) {
     const text = message.text
@@ -9,6 +24,44 @@ function flow(message, originalApiRequest) {
     }
     if (text === 'about') {
         // TODO
+    }
+    if (text === 'subscribe') {
+        // TODO: if bot framework add type AdaptiveCard, then I can remove the stupid `addReceipt`
+        const ask = new skypeTemplate.Carousel(msg.ASK_PLATFORM, msg.ASK_PLATFORM).addReceipt()
+        projectPlatforms.forEach((name) => {
+            ask.addButton(name, name, 'imBack')
+        })
+
+        return ask.get()
+    }
+
+    // Reply
+    // user select a platform from command 'subscribe'
+    if (projectPlatforms.indexOf(text) !== -1) {
+        return msg.ASK_URL + '\n\ne.g. ' + msg.EXAMPLE_URL[text]
+    }
+
+    /* Skype will send such a stupid thing when user input a URL ...
+     * '<a href="https://github.com/torvalds/linux">https://github.com/torvalds/linux</a>'
+     * so I need to sanitize here. */
+    let sanitized_text
+    if (text.startsWith('<a')) {
+        sanitized_text = text.split('>')[1].split('<')[0]
+    }
+
+    // user input a url to subscribe
+    if (site.isUrl(sanitized_text)) {
+        const url = sanitized_text
+        const platform = site.getPlatformByUrl(sanitized_text)
+        if (!platform) {
+            return msg.URL_NOTCORRECT
+        }
+        const projectName = /(?:.+)\/(.*?)(?:\/|$)/.exec(url)[1]
+
+        return site.pingSitePromise(url, platform)
+                .then(() => db.storeProjectPromise(projectName, message.sender, 'skype', platform))
+                .then(() => msg.REGISTER_FINISHED)
+                .catch(err => promiseErrorHandler(err))
     }
 
     return msg.UNKNOWN_MESSAGE
